308f085f3b7dd689b83204c146ad1c47b2d25321
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-07-30 17:24:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+/*Declare uma variável $x com o valor 5. Em seguida, incremente seu
+valor em 1 e exiba o resultado.
+$x=5;
+
+echo "O valor contido em a pós o incremento é: " . $x++ . "<br/>";
+echo "o valor atualizado de a é :" $x "<br/>";
+
+/*2-Declare uma variável $y com o valor 10. Em seguida, decrete seu valor
+em 1 e exiba o resultad
+
+$y= 10;
+echo "O valor contido em a é: $y <br/>";
+echo "O valor  contido em a após o decremento é : " . --$y;*/ 
+
+/*3-Declare uma variável $a com o valor 3. Primeiro, exiba o valor de $a e
+depois incremente usando pré-incremento e pós-incremento.*
+
+$a=3;
+echo "O valor contido em a é: .$a. <br/>";
+echo "O valor contido em a após o incremento é:" .++$a. "<br/>";*/
+
+/*4-Declare uma variável $b com o valor 8. Primeiro, exiba o valor de $b e
+depois decrete usando pré-decremento e pós-decremento.
+
+$b=8;
+echo "O valor contido em a é:" . $b . "<br/>";
+echo "O valor contido em após o decremento é:" .--$b. "<br/>";*/ 
+
+
+
+?>
